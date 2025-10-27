f49c4919ee9e901233d1bd3cbc6898ea7a34cb0b
sachinthanimesh370 <sachinthanimesh370@gmail.com>
2025-10-27 11:16:18 +0530

    Initialized Department

---
+++ ambulance/src/main/java/com/docpal/ambulance/controller/Home.java
@@ -0,0 +1,15 @@
+package com.docpal.ambulance.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class Home {
+    @GetMapping("/homeController")
+    public String homeController() {
+        return "Hello World.This is the home page of Ambulance";
+    }
+}
+
